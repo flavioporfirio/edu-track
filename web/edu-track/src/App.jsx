3a3e792af5051541aa6e4e2bf1c5e5af5a53c9f0
Flávio Porfírio <flavioporfirio86@gmail.com>
2024-04-28 14:48:57 -0300

    fix: import error

--- web/edu-track/src/App.jsx
+++ web/edu-track/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import mongoDB from "./Hooks/mongoDB";
 import useGetProfessor from "./Hooks/useGetProfessor";
 import useGetSelectedStudent from "./Hooks/useGetSelectedStudent";
 import Ajuda from "./Pages/Ajuda";
@@ -21,8 +20,6 @@ export default function App() {
   const [selectedStudent, setSelectedStudent] = useState(null);
 
   const { user } = useGetProfessor(ra);
-  const { user1 } = mongoDB();
-  console.log(user1);
   const { student } = useGetSelectedStudent(nome);
 
   function handleRA(RA) {
